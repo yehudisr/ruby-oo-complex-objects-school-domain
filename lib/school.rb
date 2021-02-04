45cbcfa873a1390565aebf46acf40c30eba46580
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-03 22:10:46 -0500

    commit tree 2027963a12bae56d6ae5a94d5e1d6bc5cdc0b574

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+     @name = name  
+     @roster = {}   
+    end  
+    
+ 
+
+    def add_student(student_name, grade)
+        
+        roster[grade] = student_name
+        
+    
+        if "grade" == grade 
+         roster[grade] << student_name
+        end
+        #binding.pry
+    end    
+
+end    
